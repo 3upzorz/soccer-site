86ce2cc8c92f4d97041ff3451db37948ac51b361
Trevor Hall <trevhall55@gmail.com>
2014-07-16 23:43:35 -0700

    structure changes on create-report view, added a home/away team name field

--- app/views/create-report.blade.php
+++ app/views/create-report.blade.php
@@ -14,8 +14,8 @@
 					<div class="row">
 						<div class="col-md-12">
 							<div class="form-group">
-								<label for="game_number">Game Number</label>
-								<input type="text" class="form-control" id="game_number">
+								<label for="game-number">Game Number</label>
+								<input type="text" class="form-control" id="game-number" name="gameNumber">
 							</div>
 						</div>
 					</div>
@@ -23,7 +23,7 @@
 						<div class="col-md-12">
 							<div class="form-group">
 								<label for="duty">Your Duty</label>
-								<select class="form-control" id="duty">
+								<select class="form-control" id="ref-type" name="refType">
 									<option val="center">Center</option>
 									<option val="assistant">Assistant</option>
 								</select>
@@ -38,14 +38,42 @@
 						</div>
 					</div>
 					<div class="row">
-						<div class="col-xs-6 form-group">
-							<label for="home_score">Home:</label>
-							<input class="form-control" type="number" id="home_score">
+						<div class="col-xs-6">
+							<div class="row">
+								<div class="col-md-12">
+									<label for="home-name">Home:</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-9 form-group">
+									<input class="form-control" type="text" id="home-name" name="homeName" placeholder="Home Team Name">
+								</div>
+								<div class="col-xs-3 form-group">
+									<input class="form-control" type="number" id="home-score" name="homeScore" placeholder="Goals">
+								</div>
+							</div>
 						</div>
-						<div class="col-xs-6 form-group">
-							<label for="away_score">Away:</label>
-							<input class="form-control" type="number" id="away_score">
+						<div class="col-xs-6">
+							<div class="row">
+								<div class="col-md-12">
+									<label for="away-name">Away:</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-9 form-group">
+									<input class="form-control" type="text" id="away-name" name="awayName" placeholder="Away Team Name">
+								</div>
+								<div class="col-xs-3 form-group">
+									<input class="form-control" type="number" id="away-score" name="awayScore" placeholder="Goals">
+								</div>
+							</div>
 						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Comments</h2>
+						</div>
+						<div class="col-md-12"></div>
 					</div>
 					<div class="row">
 						<div class="col-md-12">
@@ -56,16 +84,16 @@
 						<div id="issue-container" class="col-md-12">
 							<div class="row">
 								<div class="col-xs-3 form-group">
-									<label for="issue-list">Select</label>
-									<select class="form-control" class="issue-list">
+									<label for="issue-list-1">Select</label>
+									<select class="form-control issue-list" id="issue-list-1" name="issueList1">
 										<option val="def" selected disabled>Select an issue</option>
 										<option value="1">Red Card</option>
 										<option value="2">Yellow Card</option>
 									</select>
 								</div>
-								<div class="col-xs-9 form-group last-issue">
+								<div class="col-xs-9 form-group">
 									<label>Description</label>
-									<input class="form-control" placeholder="your issue here..."></input>
+									<input class="form-control" id="issue-description-1" name="issueDescription1" placeholder="your issue here..."></input>
 								</div>
 							</div>
 						</div>
--- public/js/soccer-site.js
+++ public/js/soccer-site.js
@@ -4,7 +4,9 @@
 $("#add-issue-btn").click(function(e){
 
 	e.preventDefault();
-	var row = '<div class="row"><div class="col-xs-3 form-group"><label for="issue-list">Select</label><select class="form-control" class="issue-list"><option val="def" selected disabled>Select an issue</option><option value="1">Red Card</option><option value="2">Yellow Card</option></select></div><div class="col-xs-9 form-group"><label>Description</label><input class="form-control" placeholder="your issue here..."></input></div></div>';
+	//TODO add counter to the issues-lists
+	var n = 1;
+	var row = '<div class="row"><div class="col-xs-3 form-group"><label for="issue-list-' + n +  ' ">Select</label><select class="form-control issue-list" id="issue-list-'+n+'" name="issueList'+n+'"><option val="def" selected disabled>Select an issue</option><option value="1">Red Card</option><option value="2">Yellow Card</option></select></div><div class="col-xs-9 form-group"><label>Description</label><input class="form-control" id="issue-description'+n+'" name="issueDescription'+n+'" placeholder="your issue here..."></input></div></div>';
 	$('#issue-container').append(row);
 	
 	var y = $(window).scrollTop();  //your current y position on the page
